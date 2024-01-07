f946b7b37d397eeb69752f2dff16c71eb8cd4f91
KMS322 <kms930322@naver.com>
2024-01-07 11:27:38 +0900

    240107_01

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const exceljs = require("exceljs");
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
     origin: ["http://192.168.0.5", "fe80::4009:75ff:fe3c:200"],
